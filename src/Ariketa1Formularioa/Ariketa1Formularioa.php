61aa08f57cc52708d106978fc45ecacbb9f2642c
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2024-09-23 12:47:33 +0200

    formularioak ariketa 23/09/24 commit

--- src/Ariketa1Formularioa/Ariketa1Formularioa.php
+++ src/Ariketa1Formularioa/Ariketa1Formularioa.php
@@ -4,41 +4,57 @@
 <?php
 
 $izenErrore = "";
+$emailErrore = "";
+$izena = "";
+$email = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if (empty($_GET["name"])) {
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  
+    if (empty($_POST["name"])) {
         $izenErrore = "Izena idatzi behar da";
-        
         echo "<script>alert('Izena idatzi behar da');</script>";
     } else {
-        $name = htmlspecialchars($_GET["name"]);
+        $izena = htmlspecialchars($_POST["name"]);
+    }
+
+    
+    if (empty($_POST["email"])) {
+        $emailErrore = "Emaila idatzi behar da";
+        echo "<script>alert('Emaila idatzi behar da');</script>";
+    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $emailErrore = "Emaila ez da zuzena";
+        echo "<script>alert('Emaila ez da zuzena');</script>";
+    } else {
+        $email = htmlspecialchars($_POST["email"]);
     }
 }
 
 ?>
 
-Kaixo <?php echo htmlspecialchars($_GET["name"] ?? ''); ?><br><br>
+Kaixo <?php echo $izena; ?><br><br>
 
 Hauek dira zure datuak:<br>
 
-Izena: <?php echo htmlspecialchars($_GET["name"] ?? ''); ?>
-
+Izena: <?php echo $izena; ?>
 <?php if ($izenErrore): ?>
     <span style="color:red;"><?php echo $izenErrore; ?></span>
 <?php endif; ?>
 <br>
 
-Zure emaila: <?php echo htmlspecialchars($_GET["email"] ?? ''); ?><br>
+Zure emaila: <?php echo $email; ?>
+<?php if ($emailErrore): ?>
+    <span style="color:red;"><?php echo $emailErrore; ?></span><br>
+<?php endif; ?>
 
-Telefono zenbakia: <?php echo htmlspecialchars($_GET["telefonoa"] ?? ''); ?><br>
+Telefono zenbakia: <?php echo htmlspecialchars($_POST["telefonoa"] ?? ''); ?><br>
 
-Probintzia: <?php echo htmlspecialchars($_GET["probintzia"] ?? ''); ?><br>
+Probintzia: <?php echo htmlspecialchars($_POST["probintzia"] ?? ''); ?><br>
 
-Generoa: <?php echo htmlspecialchars($_GET["generoa"] ?? ''); ?><br>
+Generoa: <?php echo htmlspecialchars($_POST["generoa"] ?? ''); ?><br>
 
-Iruzkina: <?php echo htmlspecialchars($_GET["iruzkina"] ?? ''); ?><br>
-
-Fitxategia igo: 
+Iruzkina: <?php echo htmlspecialchars($_POST["iruzkina"] ?? ''); ?><br>
 
 </body>
 </html>
