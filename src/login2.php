ee4b76cc07213a0b89252b0c05de01c98f35e7a3
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2024-10-02 10:22:39 +0200

    kaixo

--- src/login2.php
+++
@@ -1,4 +0,0 @@
-//Ariketa: login 2
-//Aurreko login ariketari erabiltzaile berriak erregistratzeko aukera eman (erregistroan erabiltzailearen argazkia ere jarri). 
-//Honek informazioa modu iraunkorrean gordetzea eskatzen du (datu-base batean adibidez).
-
